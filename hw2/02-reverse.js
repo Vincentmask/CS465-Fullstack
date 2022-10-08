40d1fa9c5b564e6d12da6c955fcc009c128e8438
Vincentmask <liangmask@gmail.com>
2022-10-08 12:38:00 -0700

    hw2

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,14 @@
 /** Exercise 02 - Reverse **/
 
 // Add your code here
+function reverse() {
+  var Userinput = document.getElementById("input").value;
+  var backward = Userinput.split("").reverse().join("");
+  if (backward.toString().length != 8)
+    document.getElementById("display").innerHTML =
+      "Error: Please input an 8-digit number";
+  else
+    document.getElementById("display").innerHTML =
+      Userinput + " --> " + backward;
+  console.log("bla");
+}
--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,56 @@
 /** Exercise 04 - API **/
 
-const url = 'https://restcountries.com/v3.1/all';
+const url = "https://restcountries.com/v3.1/all";
 
-// Add your code here
+let nf = new Intl.NumberFormat("en-US");
+
+// 2
+// const name = [];
+// const popu = [];
+
+const map = new Map();
+var pop = 0;
+let getData = (url) => {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // 1
+      // data.next !== null && getData(data.next);
+      // console.log(data);
+      // console.log("Name: " + data.altSpellings);
+      // return data.results;
+      // for (let i = 0; i < data.length; i += 1) {
+      //   str +=
+      //     i +
+      //     1 +
+      //     ". " +
+      //     data[i].name.official +
+      //     " - " +
+      //     data[i].population +
+      //     "\n";
+      // }
+
+      // 2
+      // for (let i = 0; i < data.length; i += 1) {
+      //   str.push(data[i].name.official);
+      //   pop = nf.format(data[i].population);
+      //   str.push(pop);
+      // }
+      for (let i = 0; i < data.length; i += 1) {
+        pop = nf.format(data[i].population);
+        map.set(data[i].name.common, pop);
+      }
+      var mapAsc = new Map([...map.entries()].sort());
+      var count = 1;
+      var str = "";
+      for (const [key, value] of mapAsc) {
+        str += count++ + `. ${key} = ${value} ` + "<br>";
+      }
+      document.getElementById("results").innerHTML = str;
+    })
+    .catch((error) => console.error("Error here", error));
+};
 
 getData(url);
